976a169df1c256d9a6cdd36e39a7a558b6fa7439
levxiann <levinagunawan29102002@gmail.com>
2023-06-05 22:53:09 +0700

    Front End Final Ver

--- resources/views/tour/dashboard.blade.php
+++ resources/views/tour/dashboard.blade.php
@@ -9,7 +9,15 @@
                     <h1 class="h3 mb-0 text-gray-800">Welcome {{Session::get('user')->data->name}}</h1>
                 </div>
 
-                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+			          <!-- Indicators -->
+			            <ol class="carousel-indicators">
+				            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				            <li data-target="#myCarousel" data-slide-to="1"></li>
+				            <li data-target="#myCarousel" data-slide-to="2"></li>		
+		            	</ol>
+
+                <div class="carousel slide" role="listbox">
                     <div class="carousel-inner">
                       <div class="carousel-item active">
                           <img src="{{asset('assets/img/bg_danautoba.png')}}" height="600" class="d-block w-100" alt="...">
@@ -20,19 +28,21 @@
                       <div class="carousel-item">
                         <img src="{{asset('assets/img/bg_batakshop.jpg')}}" height="600" class="d-block w-100" alt="...">
                       </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-target="#carouselExampleSlidesOnly" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                      </button>
-                      <button class="carousel-control-next" type="button" data-target="#carouselExampleSlidesOnly" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                      </button>
+                    </div>			             
                 </div>
-            </div>
-            <!-- /.container-fluid -->
 
+              <a class="carousel-control-prev" href="myCarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+
+              <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+
+            </div>          
+            
         </div>
         <!-- End of Main Content -->
 
